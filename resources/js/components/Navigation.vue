3030253115983d82084f42ecbc30c7d5565375ff
faishalar <knightd959@gmail.com>
2023-02-02 14:57:39 +0700

    tambah komponen vue

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,6 +1,9 @@
 <template>
-    <nav>
-        <a href="http://">Home</a>
-        <a href="http://">About</a>
-    </nav>
+    <v-toolbar elevation="8" color="#003333" density="compact" flat>
+        <v-toolbar-title>CODING BAHAGIA</v-toolbar-title>
+        <v-toolbar color="#003333" density="compact" flat>
+            <a href="http://" style="color:white">Home</a>
+            <a href="http://" style="color:white">About</a>
+        </v-toolbar>
+    </v-toolbar>
 </template>
