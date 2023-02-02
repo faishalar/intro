c07b25989f753d6f6096adbec159a3f640498db3
faishalar <knightd959@gmail.com>
2023-02-02 12:01:47 +0700

    Update Komponen

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,21 @@
-<template>        
+<template>
+    <Navigation/>        
     <h3>Laravel 9 + Vue 3</h3>
     <div class="menu">
         <v-btn color="primary">Button</v-btn>
         <v-alert dismissible type="success">Anda berhasil!!!</v-alert>
     </div>
-    
+    <Footer/>
 </template>
+
+<script>
+    import Navigation from './components/Navigation.vue'
+    import Footer from './components/Footer.vue'
+
+    export default{
+        components:{
+            Navigation,
+            Footer
+        }
+    }
+</script>
