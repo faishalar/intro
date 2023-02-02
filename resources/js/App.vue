d7f665ba0932ff73abbeceb44b77881292381bdc
faishalar <knightd959@gmail.com>
2023-02-02 11:41:57 +0700

    Perubahan struktur

---
+++ resources/js/App.vue
@@ -0,0 +1,8 @@
+<template>        
+    <h3>Laravel 9 + Vue 3</h3>
+    <div class="menu">
+        <v-btn color="primary">Button</v-btn>
+        <v-alert dismissible type="success">Anda berhasil!!!</v-alert>
+    </div>
+    
+</template>
