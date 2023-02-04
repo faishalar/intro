b3d3a28a222ab64427c1bb29cc11dd45165ed83f
faishalar <knightd959@gmail.com>
2023-02-04 14:41:34 +0700

    atur lebar navigasi

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,11 +1,12 @@
 <template>
-    <v-toolbar color="#E7E5E4" flat class="d-flex justify-content">
-        <v-container>
+    <v-toolbar color="#E7E5E4" flat>
+        <v-container style="max-width:19%;">
             <v-toolbar-title>CODING BAHAGIA</v-toolbar-title>
         </v-container>
         <v-container class="d-flex justify-center">
             <a href="http://" style="color:#57534E">Home</a>
-            <a href="http://" style="color:#57534E">About</a>
+            <a href="http://" style="color:#57534E">Pengalaman</a>
+            <a href="http://" style="color:#57534E">Tentang Saya</a>
         </v-container>
     </v-toolbar>
 </template>
