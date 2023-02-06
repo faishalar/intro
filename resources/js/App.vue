ee7e7d177f12d2df94c64c88897d7a79c54e80b4
faishalar <knightd959@gmail.com>
2023-02-06 15:17:57 +0700

    konten

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,16 +1,18 @@
 <template>
     <Navigation/>        
-    
+    <Depan/>
 </template>
 
 <script>
     import Navigation from './components/Navigation.vue'
     import Footer from './components/Footer.vue'
+    import Depan from './components/Depan.vue'
 
     export default{
         components:{
             Navigation,
-            Footer
+            Footer,
+            Depan
         }
     }
 </script>
