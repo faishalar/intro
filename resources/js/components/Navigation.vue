5d8e95b3a5a4dacaf784b3bb5d5a24a5c40c41c7
faishalar <knightd959@gmail.com>
2023-02-06 15:19:12 +0700

    edit navigasi

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-toolbar color="#E7E5E4" flat>
+    <v-toolbar color="white" flat>
         <v-container style="max-width:19%;">
             <v-toolbar-title>CODING BAHAGIA</v-toolbar-title>
         </v-container>
