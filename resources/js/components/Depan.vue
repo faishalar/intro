edce7cd64c49d260abe8ad911f6fd72b4f372815
faishalar <knightd959@gmail.com>
2023-02-07 11:17:52 +0700

    adding Grid

--- resources/js/components/Depan.vue
+++ resources/js/components/Depan.vue
@@ -1,5 +1,17 @@
 <template>
-    <v-container>
-        <h1>INI AKAN MENJADI ISI</h1>
-    </v-container>
+    <v-content>
+        <v-row no-gutters>
+            <v-col>
+                <v-container fluid class="d-grid">
+                    <h1>Sub Content</h1>
+                </v-container>
+            </v-col>
+            <v-col>
+                <v-container fluid class="d-grid">
+                    <h1>Content</h1>
+                </v-container>
+            </v-col>
+        </v-row>
+        
+    </v-content>
 </template>
