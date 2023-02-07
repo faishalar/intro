2c8767169adcd8781674db00fb6872de99b0fc7a
faishalar <knightd959@gmail.com>
2023-02-07 12:35:15 +0700

    adding image

--- resources/js/components/Depan.vue
+++ resources/js/components/Depan.vue
@@ -3,12 +3,15 @@
         <v-row no-gutters>
             <v-col>
                 <v-container fluid class="d-grid">
-                    <h1>Sub Content</h1>
+                    <h1>Hai ini adalah web perkenalan diri</h1>
+                </v-container>
+                <v-container fluid class="d-grid">
+                    <p>Hai ini adalah web perkenalan diri</p>
                 </v-container>
             </v-col>
             <v-col>
-                <v-container fluid class="d-grid">
-                    <h1>Content</h1>
+                <v-container fluid class="d-grid" start>
+                    <v-img :src="'../storage/img/profil.jpg'" :aspect-ratio="1" max-height="500"></v-img>
                 </v-container>
             </v-col>
         </v-row>
