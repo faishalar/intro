bc7aa367b23cf63828f56eff5b8a6e10b417b0a6
faishalar <knightd959@gmail.com>
2023-02-07 15:04:06 +0700

    Add Image & Add Space Between Tags

--- resources/js/components/Depan.vue
+++ resources/js/components/Depan.vue
@@ -1,16 +1,16 @@
 <template>
     <v-content>
-        <v-row no-gutters>
-            <v-col>
-                <v-container fluid class="d-grid">
-                    <h1>Hai ini adalah web perkenalan diri</h1>
+        <v-row no-gutters justify="end" class="mt-7">
+            <v-col align-self="center">
+                <v-container fluid class="ma-0 pa-0">
+                    <h1 style="text-align:right;">Hai ini adalah web perkenalan diri</h1>
                 </v-container>
-                <v-container fluid class="d-grid">
-                    <p>Hai ini adalah web perkenalan diri</p>
+                <v-container fluid class="ma-0 pa-0">
+                    <p style="text-align:right;">Mohammad Faishal Amri</p>
                 </v-container>
             </v-col>
             <v-col>
-                <v-container fluid class="d-grid" start>
+                <v-container fluid class="pa-0 ma-0">
                     <v-img :src="'../storage/img/profil.jpg'" :aspect-ratio="1" max-height="500"></v-img>
                 </v-container>
             </v-col>
