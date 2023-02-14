c7bbda2ad95e0f6b1e50927eb26bbb51c7ed7620
faishalar <knightd959@gmail.com>
2023-02-14 14:50:00 +0700

    adding vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,15 +2,43 @@ import './bootstrap';
 import {createApp} from 'vue'
 import App from './App.vue'
 
+import Depan from './pages/Depan.vue'
+import Pengalaman from './pages/Pengalaman.vue'
+import About from './pages/About.vue'
+import { createWebHistory, createRouter } from 'vue-router';
+
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+
+
+const routes=[
+  {
+    path:'/',
+    component:Depan
+  },
+  {
+    path:'/pengalaman',
+    component:Pengalaman
+  },
+  {
+    path:'/tentang-saya',
+    component:About
+  }
+]
+
+const router=new createRouter({
+  routes,
+  history : createWebHistory()
+})
+
 const vuetify = createVuetify({
   components,
   directives,
 })
-
-createApp(App).use(vuetify).mount('#app')
+const app =createApp(App);
+app.use(router);
+app.use(vuetify).mount('#app');
