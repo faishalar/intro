79f73284ee5eebed68c6dda95662b628274a0af5
faishalar <knightd959@gmail.com>
2023-02-14 14:32:36 +0700

    page folder

---
+++ resources/js/pages/Depan.vue
@@ -0,0 +1,19 @@
+<template>
+   
+        <v-row no-gutters justify="end" class="mt-7">
+            <v-col align-self="center">
+                <v-container fluid class="ma-0 pa-0">
+                    <h1 style="text-align:right;">Hai ini adalah web perkenalan diri</h1>
+                </v-container>
+                <v-container fluid class="ma-0 pa-0">
+                    <p style="text-align:right;">Mohammad Faishal Amri</p>
+                </v-container>
+            </v-col>
+            <v-col>
+                <v-container fluid class="pa-0 ma-0">
+                    <v-img :src="'../storage/img/profil.jpg'" :aspect-ratio="1" max-height="500"></v-img>
+                </v-container>
+            </v-col>
+        </v-row>
+      
+</template>
---
+++ resources/js/pages/Pengalaman.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Pengalaman konten</h1>
+</template>
