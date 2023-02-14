6539d1dd1ea5254ffb53f5e1b2a3070848bfc62e
faishalar <knightd959@gmail.com>
2023-02-14 15:12:00 +0700

    reworked design for button and title

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,9 +1,9 @@
 <template>
-    <v-toolbar color="white" flat>
-        <v-container style="max-width:19%;">
-            <v-toolbar-title style="text-align:center; text-decoration:none;"><a href="/" style="color:#57534E">HELLO WORLD</a></v-toolbar-title>
+    <v-toolbar color="white" flat class="pa-3">
+        <v-container style="max-width:35%;">
+            <v-toolbar-title style="text-align:center; text-decoration:none;"><a href="/" style="color:#57534E;font-weight:bold">HELLO WORLD</a></v-toolbar-title>
         </v-container>
-        <v-container class="d-flex justify-start ml-10 pl-7">
+        <v-container class="d-flex justify-end mr-15 pr-16">
             <router-link to="/" style="color:#57534E">Home</router-link>
             <router-link to="/pengalaman" style="color:#57534E">Pengalaman</router-link>
             <router-link to="/tentang-saya" style="color:#57534E">Tentang Saya</router-link>
