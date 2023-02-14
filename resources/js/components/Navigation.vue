82d630e8f46aaa59e074ae3829ff77cecbde4fbb
faishalar <knightd959@gmail.com>
2023-02-14 14:37:21 +0700

    using vue router

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,12 +1,12 @@
 <template>
     <v-toolbar color="white" flat>
         <v-container style="max-width:19%;">
-            <v-toolbar-title style="text-align:center; text-decoration:none;"><a href="http://" style="color:#57534E">HELLO WORLD</a></v-toolbar-title>
+            <v-toolbar-title style="text-align:center; text-decoration:none;"><a href="/" style="color:#57534E">HELLO WORLD</a></v-toolbar-title>
         </v-container>
         <v-container class="d-flex justify-start ml-10 pl-7">
-            <a href="http://" style="color:#57534E">Home</a>
-            <a href="http://" style="color:#57534E">Pengalaman</a>
-            <a href="http://" style="color:#57534E">Tentang Saya</a>
+            <router-link to="/" style="color:#57534E">Home</router-link>
+            <router-link to="/pengalaman" style="color:#57534E">Pengalaman</router-link>
+            <router-link to="/tentang-saya" style="color:#57534E">Tentang Saya</router-link>
         </v-container>
     </v-toolbar>
 </template>
