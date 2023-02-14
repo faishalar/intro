8b5b4cc2138662c48ea0abd2682851d04ede6b89
faishalar <knightd959@gmail.com>
2023-02-14 14:34:20 +0700

    using vue router

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,19 +1,22 @@
 <template>
-    <Navigation/>        
-    <Depan/>
+    <Navigation/>   
+    <router-view></router-view>
     <Footer/>
 </template>
 
 <script>
     import Navigation from './components/Navigation.vue'
     import Footer from './components/Footer.vue'
-    import Depan from './components/Depan.vue'
 
     export default{
         components:{
             Navigation,
-            Footer,
-            Depan
+            Footer
+        },
+        data(){
+            return{
+                title:'laravel 9'
+            }
         }
     }
 </script>
