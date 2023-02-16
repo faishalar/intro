b52473c812cbc2c253b2ec3f717bda25f93c2e5a
faishalar <knightd959@gmail.com>
2023-02-16 09:06:09 +0700

    Pengalaman konten

--- resources/js/pages/Pengalaman.vue
+++ resources/js/pages/Pengalaman.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Pengalaman konten</h1>
+    <v-container>
+        <v-row justify="center" align="center">   
+                <h1 class="pa-2 ma-2">Pengalaman konten</h1>
+        </v-row>
+    </v-container>
+    
 </template>
