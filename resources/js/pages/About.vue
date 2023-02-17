7c8203bae912607ec09817345bdddd889a768e24
faishalar <knightd959@gmail.com>
2023-02-17 10:29:42 +0700

    adding vue motion

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -1,7 +1,9 @@
 <template>
     <v-container>
         <v-row align="center" justify="center">
-            <h1>About konten</h1>
+            <div class="target" v-motion-slide-visible-bottom>
+                <h1>About konten</h1>
+            </div>
         </v-row>
     </v-container>
 </template>
--- resources/js/pages/Pengalaman.vue
+++ resources/js/pages/Pengalaman.vue
@@ -1,7 +1,9 @@
 <template>
     <v-container>
         <v-row justify="center" align="center">   
+            <div class="target" v-motion-slide-visible-bottom>
                 <h1 class="pa-2 ma-2">Pengalaman konten</h1>
+            </div>
         </v-row>
     </v-container>
     
