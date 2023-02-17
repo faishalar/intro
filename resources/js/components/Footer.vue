7021ee3c2a654504793fe3bfef37d3603b416bd2
faishalar <knightd959@gmail.com>
2023-02-17 10:30:19 +0700

    adding mdi icon

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,6 +1,12 @@
 <template>
     <footer style="background-color:#0f0e17;margin-top:20pt">
         <v-container>
+            <v-row align="center" justify="center" class="ma-5">
+                <i class="mdi-reddit mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+                <i class="mdi-github mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+                <i class="mdi-twitter mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+                <i class="mdi-gmail mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+                </v-row>
             <p style="text-align:center; color:white" class="mt-4">&copy; 2022 - Mohammad Faishal Amri</p>
         </v-container>
     </footer>
