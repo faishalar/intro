391fe7c8acec582249c06a35db1119e7b663dfaf
faishalar <knightd959@gmail.com>
2023-02-20 14:53:27 +0700

    adding content

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -1,9 +1,32 @@
 <template>
     <v-container>
-        <v-row align="center" justify="center">
-            <div class="target" v-motion-slide-visible-bottom>
-                <h1>About konten</h1>
-            </div>
+        <v-row>
+            <v-col>
+                <div v-motion-pop-visible>
+                    <h1 style="text-align:center;">Tentang Saya</h1>
+                </div>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="5">
+                <v-sheet>
+                    <div v-motion-pop-visible>
+                        <h1>Biodata</h1>
+                    </div>
+                </v-sheet>
+            </v-col>
+            <v-col>
+                <v-sheet>
+                    <div>
+                        <h1>Pendidikan</h1>
+                    </div>
+                </v-sheet>
+                <v-sheet>
+                    <div>
+                        <h1>Keahlian</h1>
+                    </div>
+                </v-sheet>
+            </v-col>
         </v-row>
     </v-container>
 </template>
