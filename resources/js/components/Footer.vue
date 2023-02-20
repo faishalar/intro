246bf3f5f301d4f90e31ced29b9739d35942e622
faishalar <knightd959@gmail.com>
2023-02-20 14:52:26 +0700

    adding a elemet on icon

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -2,12 +2,18 @@
     <footer style="background-color:#0f0e17;margin-top:20pt">
         <v-container>
             <v-row align="center" justify="center" class="ma-5">
-                
+                <a href="https://www.google.com" target="_blank">
                     <i class="mdi-reddit mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
-
-                <i class="mdi-github mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
-                <i class="mdi-twitter mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
-                <i class="mdi-gmail mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+                </a>
+                <a href="https://" target="_blank">
+                    <i class="mdi-github mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+                </a>                
+                <a href="https://" target="_blank">
+                    <i class="mdi-twitter mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+                </a>
+                <a href="https://" target="_blank">
+                    <i class="mdi-gmail mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+                </a>
                 </v-row>
             <p style="text-align:center; color:white" class="mt-4">&copy; 2022 - Mohammad Faishal Amri</p>
         </v-container>
