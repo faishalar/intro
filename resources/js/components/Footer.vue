f67fb85fc212a6ca8dca4ab6a7a77a90d91bfa00
faishalar <knightd959@gmail.com>
2023-02-20 09:32:43 +0700

    adding anchor on icon social media

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -2,7 +2,9 @@
     <footer style="background-color:#0f0e17;margin-top:20pt">
         <v-container>
             <v-row align="center" justify="center" class="ma-5">
-                <i class="mdi-reddit mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+                
+                    <i class="mdi-reddit mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
+
                 <i class="mdi-github mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
                 <i class="mdi-twitter mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
                 <i class="mdi-gmail mdi v-icon notranslate v-theme--dark v-icon--size-default text-white ma-1" aria-hidden="true"></i>
